918699246459e6211fb70e6de3feedc8de419ae4
tmzhi <tmzhi@163.com>
2016-11-21 15:04:45 +0800

    Submit new version.

---
+++ app/src/main/java/com/hcp/entities/Item.java
@@ -0,0 +1,10 @@
+package com.hcp.entities;
+
+/**
+ * Created by tmzhiPC on 2016-09-19.
+ */
+public class Item {
+    public String Segment;
+
+    public String Description;
+}
---
+++ app/src/main/java/com/hcp/entities/Organization.java
@@ -0,0 +1,12 @@
+package com.hcp.entities;
+
+/**
+ * Created by tmzhiPC on 2016-10-10.
+ */
+public class Organization {
+    public int OrgId ;
+
+    public String OrgCode ;
+
+    public String Attribute6 ;
+}
